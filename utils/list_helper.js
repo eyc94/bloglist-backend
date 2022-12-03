635abd424459add1a3d58ad25c1a6573efe4a43c
Eric Chin <ericychin94@gmail.com>
2022-12-03 03:32:53 -0800

    Exercise 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -10,7 +10,28 @@ const totalLikes = (blogs) => {
   return likesSum;
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+  let favoriteBlog = {};
+  let mostLikesSoFar = 0;
+  for (let i = 0; i < blogs.length; i++) {
+    let currentBlogLikes = blogs[i].likes;
+    if (currentBlogLikes > mostLikesSoFar) {
+      mostLikesSoFar = currentBlogLikes;
+      favoriteBlog = blogs[i];
+    }
+  }
+  const condensedBlog = {};
+  condensedBlog.title = favoriteBlog.title;
+  condensedBlog.author = favoriteBlog.author;
+  condensedBlog.likes = favoriteBlog.likes;
+  return condensedBlog;
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
