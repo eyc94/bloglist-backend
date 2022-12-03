9383ab5453f5d18f1d00cad763fc508067ec7d8e
Eric Chin <ericychin94@gmail.com>
2022-12-03 03:24:35 -0800

    Exercise 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  let likesSum = 0;
+  for (let i = 0; i < blogs.length; i++) {
+    likesSum += blogs[i].likes;
+  }
+  return likesSum;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
