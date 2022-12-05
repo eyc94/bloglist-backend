dfc620fcded6b7bca13292bb9ebe8988114fb383
Eric Chin <ericychin94@gmail.com>
2022-12-05 14:18:22 -0800

    Exercise 4.13

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -112,6 +112,24 @@ test('blog without the url property is not added', async () => {
   expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
 });
 
+describe('deletion of a blog', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
+
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204);
+    
+    const blogsAtEnd = await helper.blogsInDb();
+
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
+
+    const titles = blogsAtEnd.map(r => r.title);
+    expect(titles).not.toContain(blogToDelete.title);
+  });
+});
+
 afterAll(() => {
   mongoose.connection.close();
 });
