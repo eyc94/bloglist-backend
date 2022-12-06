65974e18c0daa6aa7cebd08d397f74b13e570f34
Eric Chin <ericychin94@gmail.com>
2022-12-05 16:40:20 -0800

    Exercise 4.16

--- controllers/users.js
+++ controllers/users.js
@@ -5,6 +5,31 @@ const User = require('../models/user');
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body;
 
+  if (!username || !password) {
+    return response.status(400).json({
+      error: 'Username or password missing'
+    });
+  }
+
+  if (password.length < 3) {
+    return response.status(400).json({
+      error: 'Password length must be at least 3 characters long'
+    });
+  }
+
+  if (username.length < 3) {
+    return response.status(400).json({
+      error: 'Username length must be at least 3 characters long'
+    });
+  }
+
+  const existingUser = await User.findOne({ username });
+  if (existingUser) {
+    return response.status(400).json({
+      error: 'Username must be unique'
+    });
+  }
+
   const saltRounds = 10;
   const passwordHash = await bcrypt.hash(password, saltRounds);
 
