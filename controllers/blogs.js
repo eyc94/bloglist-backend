af83f585d49425634134faf5cb558813ac17767c
Eric Chin <ericychin94@gmail.com>
2022-12-07 01:04:14 -0800

    Exercise 5.8

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -64,16 +64,20 @@ blogsRouter.put('/:id', async (request, response) => {
   const body = request.body;
 
   const blog = {
+    user: body.user,
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes,
   };
 
-  const updatedNote = await Blog.findByIdAndUpdate(
-    request.params.id, blog, { new: true }
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    request.params.id,
+    blog,
+    { new: true, runValidators: true, context: 'query' }
   );
-  response.json(updatedNote);
+  console.log(updatedBlog);
+  response.json(updatedBlog);
 });
 
 module.exports = blogsRouter;
