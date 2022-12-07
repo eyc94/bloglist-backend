2a68be21cba25fb6cca9173f026f8cdc0c175218
Eric Chin <ericychin94@gmail.com>
2022-12-07 13:57:48 -0800

    Added user

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -7,12 +7,14 @@ const initialBlogs = [
     author: "Michael Chan",
     url: "https://reactpatterns.com/",
     likes: 7,
+    user: "6391077656d829e8e48d318b",
   },
   {
     title: "Go To Statement Considered Harmful",
     author: "Edsger W. Dijkstra",
     url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
     likes: 5,
+    user: "6391077656d829e8e48d318b",
   },
 ];
 
